e6126499254351d2e6afb6aafe1f414f6a404ec2
Brian Wynn <brian.wynn83@gmail.com>
2016-10-27 20:48:31 -0700

    Added bike controller dependency.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-angular.module('ngDragDropApp', ['ui.router','dndLists', 'api.users', 'api.bikes', 'components.users'])
+angular.module('ngDragDropApp', ['ui.router','dndLists', 'api.users', 'api.bikes', 'components.users', 'components.bikes'])
     .config(function($urlRouterProvider) {
-        $urlRouterProvider.otherwise('/users');
+        $urlRouterProvider.otherwise('/bikes');
     });
